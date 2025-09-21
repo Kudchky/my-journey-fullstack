2cd8c4205af01fb7cb99f0ce23507bb14a419b8d
Pol Castillo <rpcastilloy@gmail.com>
2025-09-21 17:15:23 -0500

    feat (if-else):  Implement Exercise03 to verify if number is positive or negative.

--- 01-fundamentals-of-java/exercises/week01/Exercise03.java
+++ 01-fundamentals-of-java/exercises/week01/Exercise03.java
@@ -1,7 +1,21 @@
 package exercises.week01;
 
+import exercises.utils.Input;
+
 public class Exercise03 {
     public static void main(String[] args) {
+        int number;
+
+        IO.println("Enter a number: ");
+        number = Input.numInteger();
+
+        if (number > 0) {
+            IO.println("Number is positive");
+        } else if (number < 0) {
+            IO.println("Number is negative");
+        } else {
+            IO.println("Number is zero");
+        }
 
     }
 }
