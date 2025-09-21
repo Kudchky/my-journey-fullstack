5a02f0649337ead990ce59283364f8a89e6d36aa
Pol Castillo <rpcastilloy@gmail.com>
2025-09-21 13:20:25 -0500

    Finished calculation exercise of the area of a circle

--- 01-fundamentals-of-java/exercises/week01/Exercise04.java
+++ 01-fundamentals-of-java/exercises/week01/Exercise04.java
@@ -4,7 +4,12 @@ import exercises.utils.Input;
 
 public class Exercise04 {
     void main(String[] args) {
-        double radius = Input.numDouble();
+        double radius;
+        final double PI = 3.14159;
 
+        IO.println("Input radius of the circle: ");
+        radius = Input.numDouble();
+
+        IO.println("The area of circle is: " + PI * radius * radius);
     }
 }
