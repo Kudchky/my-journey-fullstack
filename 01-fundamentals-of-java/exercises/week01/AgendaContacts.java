8ff7c9e8e5fb707820012d38af9ed47d2c4fa148
Pol Castillo <rpcastilloy@gmail.com>
2025-09-28 21:26:40 -0500

    AgendaContacts

---
+++ 01-fundamentals-of-java/exercises/week01/AgendaContacts.java
@@ -0,0 +1,47 @@
+package exercises.week01;
+/*
+* Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización
+ *   y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar,
+ *   y a continuación los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no numéricos y con más
+ *   de 11 dígitos (o el número de dígitos que quieras).
+ * - También se debe proponer una operación de finalización del programa.
+**PLANTILLA PARA CUALQUIER PROBLEMA**
+
+1. **ANALIZAR**:
+    Entrada:
+        Opcion del menu a elegir
+
+    Salida:
+        resultado de la opcion escogida ya sea una busqueda, insercion, actualizacion o borrar.
+        *
+    Casos clave o limite:
+                -cada ingreso debe tener nombre y telefono
+                -validacion de numeros de telefono valido y con
+                 mas de 11 digitos.
+                -Opcion de salida del programa.
+
+2. **DIVIDIR**: Descomponer en subproblemas más pequeños
+*               -crear un menu de opciones.
+*               -crear funciones para cada opcion
+*               -validar entrada de datos (nombres y celular)
+*               -mostrar resultado.
+
+3. **DATOS**: Estructuras necesarias (arrays, objetos, etc.)
+*               -while, arrays, case
+*               -funciones, etc
+
+4. **ALGORITMO**: Lógica para cada caso
+
+5. **INTEGRAR**: Unir todas las partes
+*/
+
+public class AgendaContacts {
+    void main() {
+
+    }
+
+}
